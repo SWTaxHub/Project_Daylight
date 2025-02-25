b965b67fabf8ce9ea562c99622e21efe4c09266e
samsmitt98 <samsmith.hf@gmail.com>
2025-02-25 14:20:50 +1100

    Amendment of file paths

--- UniSC _ CodeOnly/run_files_in_sequence.py
+++ UniSC _ CodeOnly/run_files_in_sequence.py
@@ -72,17 +72,25 @@ def run_scripts_in_sequence(python_files):
 if __name__ == "__main__":
     # Your existing python_files list
     python_files = [
-        r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\UniSC _ PaulsCode\Project files\2- Weekend Penalties and Overtime\m4_weekend_and_overtime_penalties_working_file.py",
-        r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\UniSC _ PaulsCode\Project files\2- Weekend Penalties and Overtime\m4.1_Casual_Academics.py",
-        r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\UniSC _ PaulsCode\Project files\2- Weekend Penalties and Overtime\m5_ot_span_weekend.py",
-        r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\UniSC _ PaulsCode\Project files\2- Weekend Penalties and Overtime\m6_ot_daily_weekly.py",
-        r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\UniSC _ PaulsCode\Project files\2- Weekend Penalties and Overtime\m7_meal_allowance_calcs.py",
-        r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\UniSC _ PaulsCode\Project files\3 - Minimum Engagement\me2_timesheet_student.py",
-        r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\UniSC _ PaulsCode\Project files\3 - Minimum Engagement\me3_timesheet_student_perm.py",
-        r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\UniSC _ PaulsCode\Project files\3 - Minimum Engagement\me4_timesheet_me_calc_v2.py",
-        r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\UniSC _ PaulsCode\Project files\3 - Minimum Engagement\me5_Super_UnderPayCalcs.py",
-        r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\UniSC _ PaulsCode\Project files\3 - Minimum Engagement\me5b_OT_recalc.py",
-        r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\UniSC _ PaulsCode\Project files\3 - Minimum Engagement\test_final_underpayment.py"
+        r"C:\Git\Project_Daylight\UniSC _ CodeOnly\Project files\2- Weekend Penalties and Overtime\m4_weekend_and_overtime_penalties_working_file.py",
+        r"C:\Git\Project_Daylight\UniSC _ CodeOnly\Project files\2- Weekend Penalties and Overtime\m4.1_Casual_Academics.py",
+        # r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\UniSC _ PaulsCode\Project files\2- Weekend Penalties and Overtime\m5_ot_span_weekend.py",
+        r"C:\Git\Project_Daylight\UniSC _ CodeOnly\Project files\2- Weekend Penalties and Overtime\m5_ot_span_weekend.py", 
+        # r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\UniSC _ PaulsCode\Project files\2- Weekend Penalties and Overtime\m6_ot_daily_weekly.py",
+        r"C:\Git\Project_Daylight\UniSC _ CodeOnly\Project files\2- Weekend Penalties and Overtime\m6_ot_daily_weekly.py",
+        # r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\UniSC _ PaulsCode\Project files\2- Weekend Penalties and Overtime\m7_meal_allowance_calcs.py",
+        r"C:\Git\Project_Daylight\UniSC _ CodeOnly\Project files\2- Weekend Penalties and Overtime\m7_meal_allowance_calcs.py",
+        # r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\UniSC _ PaulsCode\Project files\3 - Minimum Engagement\me2_timesheet_student.py",
+        r"C:\Git\Project_Daylight\UniSC _ CodeOnly\Project files\3 - Minimum Engagement\me2_timesheet_student.py",
+       #r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\UniSC _ PaulsCode\Project files\3 - Minimum Engagement\me3_timesheet_student_perm.py",
+        r"C:\Git\Project_Daylight\UniSC _ CodeOnly\Project files\3 - Minimum Engagement\me3_timesheet_student_perm.py",
+        # r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\UniSC _ PaulsCode\Project files\3 - Minimum Engagement\me4_timesheet_me_calc_v2.py",
+        r"C:\Git\Project_Daylight\UniSC _ CodeOnly\Project files\3 - Minimum Engagement\me4_timesheet_me_calc_v2.py",
+        r"C:\Git\Project_Daylight\UniSC _ CodeOnly\Project files\3 - Minimum Engagement\me5_Super_UnderPayCalcs.py",
+        # r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\UniSC _ PaulsCode\Project files\3 - Minimum Engagement\me5b_OT_recalc.py",
+        r"C:\Git\Project_Daylight\UniSC _ CodeOnly\Project files\3 - Minimum Engagement\me5_Super_UnderPayCalcs.py",
+        # r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\UniSC _ PaulsCode\Project files\3 - Minimum Engagement\test_final_underpayment.py"
+        r"C:\Git\Project_Daylight\UniSC _ CodeOnly\Project files\3 - Minimum Engagement\test_final_underpayment.py"
     ]
         
     run_scripts_in_sequence(python_files)
